8e22096bc41ac5b88587ab77e885077ac3c60d5c
mmarci96 <sarosdimarci@gmail.com>
2025-07-09 01:08:40 +0200

    experimenting with sending secret from localstorage on request

--- pkg/handlers/ws_handler.go
+++ pkg/handlers/ws_handler.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"sync"
@@ -11,45 +10,78 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type Client struct {
+	conn     *websocket.Conn
+	username string
+	isRoot   bool
+}
+
 type ClientManager struct {
-	clients map[*websocket.Conn]bool
+	clients []*Client
 	lock    sync.Mutex
 }
 
 func NewClientManager() *ClientManager {
 	return &ClientManager{
-		clients: make(map[*websocket.Conn]bool),
+		clients: []*Client{},
 	}
 }
 
-func (cm *ClientManager) Add(conn *websocket.Conn) {
+func (cm *ClientManager) Add(client *Client) {
 	cm.lock.Lock()
 	defer cm.lock.Unlock()
-	cm.clients[conn] = true
+	cm.clients = append(cm.clients, client)
 	log.Printf("Client connected. Total: %d", len(cm.clients))
 }
 
 func (cm *ClientManager) Remove(conn *websocket.Conn) {
 	cm.lock.Lock()
 	defer cm.lock.Unlock()
-	delete(cm.clients, conn)
+	for i, c := range cm.clients {
+		if c.conn == conn {
+			c.conn.Close()
+			cm.clients = append(cm.clients[:i], cm.clients[i+1:]...)
+			break
+		}
+	}
 	log.Printf("Client disconnected. Total: %d", len(cm.clients))
+}
+
+func (cm *ClientManager) FindRoot() *Client {
+	for _, c := range cm.clients {
+		if c.isRoot {
+			return c
+		}
+	}
+	return nil
+}
+
+func (cm *ClientManager) FindByUsername(username string) *Client {
+	for _, c := range cm.clients {
+		if c.username == username {
+			return c
+		}
+	}
+	return nil
 }
 
 func (cm *ClientManager) Broadcast(message string) {
 	cm.lock.Lock()
 	defer cm.lock.Unlock()
-	for conn := range cm.clients {
-		if err := conn.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
-			log.Println("Write error:", err)
-			e := conn.Close()
-			if e != nil {
-				log.Fatal("Error closing connection. Is this even possible?")
-				return
-			}
-			delete(cm.clients, conn)
+
+	aliveClients := []*Client{}
+
+	for _, client := range cm.clients {
+		err := client.conn.WriteMessage(websocket.TextMessage, []byte(message))
+		if err != nil {
+			log.Println("Broadcast write error:", err)
+			client.conn.Close()
+			continue
 		}
+		aliveClients = append(aliveClients, client)
 	}
+
+	cm.clients = aliveClients
 }
 
 var upgrader = websocket.Upgrader{
@@ -65,7 +97,12 @@ func WsHandler(cm *ClientManager, hostUser string) gin.HandlerFunc {
 			log.Println("WebSocket upgrade failed:", err)
 			return
 		}
-		cm.Add(conn)
+		defer conn.Close()
+
+		client := &Client{
+			conn: conn,
+		}
+		cm.Add(client)
 		defer cm.Remove(conn)
 
 		for {
@@ -74,22 +111,65 @@ func WsHandler(cm *ClientManager, hostUser string) gin.HandlerFunc {
 				log.Println("Read error:", err)
 				break
 			}
-			log.Printf("Received: %s", msg)
 
 			var parsed map[string]any
-			if err := json.Unmarshal(msg, &parsed); err == nil {
-				if parsed["type"] == "login" {
-					username := parsed["username"].(string)
-					isRoot := username == hostUser
-					response := fmt.Sprintf("Login received. User: %s, Root: %v", username, isRoot)
-					e := conn.WriteMessage(websocket.TextMessage, []byte(response))
-					if e != nil {
-						return
-					}
+			if err := json.Unmarshal(msg, &parsed); err != nil {
+				log.Println("Invalid JSON:", string(msg))
+				continue
+			}
+
+			switch parsed["type"] {
+			case "login":
+				username := parsed["username"].(string)
+				isRoot := username == hostUser
+				client.username = username
+				client.isRoot = isRoot
+
+				response := map[string]any{
+					"type":     "login_ack",
+					"username": username,
+					"root":     isRoot,
+				}
+				respJSON, _ := json.Marshal(response)
+				conn.WriteMessage(websocket.TextMessage, respJSON)
+
+			case "getsecret":
+				if client.isRoot {
 					continue
 				}
+				root := cm.FindRoot()
+				if root != nil {
+					request := map[string]any{
+						"type":     "approve_secret",
+						"username": client.username,
+					}
+					reqJSON, _ := json.Marshal(request)
+					root.conn.WriteMessage(websocket.TextMessage, reqJSON)
+				} else {
+					errResp := map[string]any{
+						"type":  "error",
+						"error": "No root user connected",
+					}
+					errJSON, _ := json.Marshal(errResp)
+					conn.WriteMessage(websocket.TextMessage, errJSON)
+				}
+
+			case "secret_response":
+				targetUser := parsed["username"].(string)
+				secret := parsed["secret"].(string)
+				targetClient := cm.FindByUsername(targetUser)
+				if targetClient != nil {
+					result := map[string]any{
+						"type":   "secret_result",
+						"secret": secret,
+					}
+					resultJSON, _ := json.Marshal(result)
+					targetClient.conn.WriteMessage(websocket.TextMessage, resultJSON)
+				}
+
+			default:
+				log.Println("Unhandled type:", parsed["type"])
 			}
-			cm.Broadcast(fmt.Sprintf("Echo: %s", msg))
 		}
 	}
 }
